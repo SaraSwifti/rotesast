ef28ce8ef0bdcfd584320c29b31628f63b0b42bc
Sara Taylor <sara.swifti@gmail.com>
2021-03-24 10:18:14 -0400

    changed teh produciton env variable

--- config/config.js
+++ config/config.js
@@ -15,7 +15,7 @@ module.exports =
     "dialect": "mysql"
   },
     "production": {
-      "use_env_variable": "JAWSDB_URL",
+      "use_env_variable": "PROJECT2_URL",
       "dialect": "mysql"
     }
   
